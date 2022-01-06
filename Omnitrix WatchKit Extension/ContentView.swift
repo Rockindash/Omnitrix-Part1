07ef3960a84e118c75ce6cacace689716d452dbb
Amol Kumar <amolkmr10@gmail.com>
2022-01-06 19:38:31 +0530

    Initial Commit

---
+++ Omnitrix WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Omnitrix WatchKit Extension
+//
+//  Created by Amol Kumar on 2022-01-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Omnitrix WatchKit Extension/OmnitrixApp.swift
@@ -0,0 +1,19 @@
+//
+//  OmnitrixApp.swift
+//  Omnitrix WatchKit Extension
+//
+//  Created by Amol Kumar on 2022-01-06.
+//
+
+import SwiftUI
+
+@main
+struct OmnitrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
