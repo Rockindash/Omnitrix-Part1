9ba04a4900492c0b97b067944faa72b702384729
Amol Kumar <amolkmr10@gmail.com>
2022-01-07 22:23:21 +0530

    Project init

--- Omnitrix WatchKit Extension/ContentView.swift
+++ Omnitrix WatchKit Extension/ContentView.swift
@@ -7,10 +7,37 @@
 
 import SwiftUI
 
+enum Aliens: Int {
+    case Diamondhead
+    case Cannonbolt
+    case XLR8
+    case Fireblast
+    case Fourarms
+    case GrayMatter
+    case Ripjaws
+    case Upgrade
+    case Wildmutt
+    case Stinkfly
+}
+
 struct ContentView: View {
+    @State var scrollAmount = 0.0
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        ZStack {
+            Image("Watchface")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .rotationEffect(.degrees(scrollAmount*90))
+            
+            Image("\(Aliens(rawValue: Int(scrollAmount))!)")
+                .resizable()
+                .padding(.all, 40)
+                .aspectRatio(contentMode: .fit)
+        }
+        .offset(y: 15)
+        .focusable(true)
+        .digitalCrownRotation($scrollAmount, from: 0, through: 9, by: 1, sensitivity: .low, isContinuous: false, isHapticFeedbackEnabled: true)
     }
 }
 
